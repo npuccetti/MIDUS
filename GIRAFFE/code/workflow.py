4f31705dd772746316307a59376678098089f0fb
Nikki Puccetti <napuccetti9@gmail.com>
2019-04-03 17:36:57 -0400

    incorporating w.in sub registration

--- GIRAFFE/code/workflow.py
+++ GIRAFFE/code/workflow.py
@@ -8,6 +8,7 @@ import nipype.pipeline as pe
 import nipype.interfaces.fsl as fsl
 import nipype.interfaces.io as io
 import nipype.interfaces.afni as afni
+import nipype.interfaces.ants as ants
 
 #Wraps command **bet**
 fsl_BET = pe.Node(interface = fsl.BET(), name='fsl_BET')
@@ -27,10 +28,23 @@ afni_Despike = pe.Node(interface = afni.Despike(), name='afni_Despike')
 #Wraps the executable command ``3dTcat``.
 afni_TCat = pe.Node(interface = afni.TCat(), name='afni_TCat')
 
+#Wraps the executable command ``3dvolreg``.
+afni_Volreg = pe.Node(interface = afni.Volreg(), name='afni_Volreg')
+
+#Wraps the executable command ``antsRegistrationSyNQuick.sh``.
+ants_RegistrationSynQuick_anat2epi = pe.Node(interface = ants.RegistrationSynQuick(), name='ants_RegistrationSynQuick_anat2epi')
+
+#Wraps the executable command ``antsRegistration``.
+ants_Registration = pe.Node(interface = ants.Registration(), name='ants_Registration')
+
 #Create a workflow to connect all those nodes
 analysisflow = nipype.Workflow('MyWorkflow')
 analysisflow.connect(afni_TCat, "out_file", afni_Despike, "in_file")
 analysisflow.connect(afni_Unifize, "out_file", fsl_BET, "in_file")
+analysisflow.connect(afni_Despike, "out_file", afni_Volreg, "in_file")
+analysisflow.connect(fsl_BET, "out_file", ants_RegistrationSynQuick_anat2epi, "fixed_image")
+analysisflow.connect(afni_Volreg, "out_file", ants_RegistrationSynQuick_anat2epi, "moving_image")
+analysisflow.connect(ants_RegistrationSynQuick_anat2epi, "warped_image", ants_Registration, "moving_image")
 
 #Run the workflow
 plugin = 'MultiProc' #adjust your desired plugin here
